2c78b69dcc3d17ede84a4b76f878ea71d4ae9149
noralam <nnalam50@gmail.com>
2026-07-25 22:41:53 +0600

    Add compiled build assets

---
+++ build/chatbot-widget.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-element', 'wp-i18n'), 'version' => '9620061ee32357c65fd4');
---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '7603744d46106982b3a5');
